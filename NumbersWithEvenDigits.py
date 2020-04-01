46c72637e7912b4804a0142cf7cfe43ab5716ecd
Momin-C <hellomomins@yahoo.com>
2020-03-31 23:43:38 -0400

    Modified to improve efficiency

--- NumbersWithEvenDigits.py
+++ NumbersWithEvenDigits.py
@@ -1,6 +1,9 @@
 #SOURCE: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 nums = [12,345,2,6,7896]
+"""
 oc = 0
 for i in nums:
     if len(str(i))%2 ==0: oc+=1
 return oc
+"""
+print (sum(map(lambda i: 1 if i%2==0 else 0,nums)))
