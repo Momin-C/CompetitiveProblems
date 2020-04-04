18740da86112930d56ebb753d66558ba29c26702
Momin-C <hellomomins@yahoo.com>
2020-04-04 19:57:35 -0400

    Finished another problem

---
+++ BinaryToInt.py
@@ -0,0 +1,2 @@
+head = [1,0,1]
+print (''.join(map(lambda x: x, head)))
---
+++ InsertElement.py
@@ -0,0 +1,16 @@
+#PROBLEM: 1389
+#SOURCE: https://leetcode.com/problems/create-target-array-in-the-given-order/
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+
+def createTargetArray(nums, index):
+    """
+    :type nums: List[int]
+    :type index: List[int]
+    :rtype: List[int]
+    """
+    lst = []
+    for i in range (len(nums)):
+        lst.insert(index[i],nums[i])
+    return lst
+print (createTargetArray(nums, index))
