a0abda5807b9dab4a5c1bafa8f66d44bdaccae44
Momin-C <hellomomins@yahoo.com>
2020-04-05 18:06:08 -0400

    Finished another problem

---
+++ 1323 Maximum 69 Number.py
@@ -0,0 +1,13 @@
+num = 9999 # 9999
+
+def maximum69Number (num):
+    num = str(num)
+    return int(str(num).replace("6","9",1))
+    """
+    r = num.find("6")
+    if r != -1:
+        return int(num[:r] + "9" + num[r+1:])
+    else:
+        return int(num)
+    """
+print (maximum69Number(num))
