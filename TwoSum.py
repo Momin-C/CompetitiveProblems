0fcdb654de3decd215c69e9d554ee19b7625d8c1
Momin-C <hellomomins@yahoo.com>
2020-04-04 21:32:12 -0400

    Modified problem

--- TwoSum.py
+++ TwoSum.py
@@ -12,12 +12,3 @@ def twoSum(nums, target):
                 lst.extend([i,num])
                 break
     return lst
-print (twoSum(nums0,target0))
-print (twoSum(nums1,target1))
-print (twoSum(nums2,target2))
-print (twoSum(nums3,target3))
-print (twoSum(nums4,target4))
-print (twoSum(nums5,target5))
-print (twoSum(nums6,target6))
-print (twoSum(nums7,target7))
-print (twoSum(nums8,target8))
