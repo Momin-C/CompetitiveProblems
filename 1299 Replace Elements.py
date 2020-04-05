3f2dd403f4557501b18edd073dfed1368ebab913
Momin-C <hellomomins@yahoo.com>
2020-04-04 23:35:34 -0400

    Finished another problem

---
+++ 1299 Replace Elements.py
@@ -0,0 +1,16 @@
+#PROBLEM: 1299
+#SOURCE: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+arr = [17,18,5,4,6,1]
+
+def replaceElements(arr):
+    """
+    :type arr: List[int]
+    :rtype: List[int]
+    """
+    for i in range(len(arr)):
+        if i!= len(arr)-1:
+            arr[i] = max(arr[i+1:])
+    arr[-1] = -1
+    return arr
+
+print (replaceElements(arr))
--- BinaryToInt.py
+++
@@ -1,2 +0,0 @@
-head = [1,0,1]
-print (''.join(map(lambda x: x, head)))
