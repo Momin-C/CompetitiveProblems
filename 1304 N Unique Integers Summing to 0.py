12ccfff49dd00b247eb1d890e94dca9ae870533d
Momin-C <hellomomins@yahoo.com>
2020-04-06 21:03:29 -0400

    Finished Problem 1304

---
+++ 1304 N Unique Integers Summing to 0.py
@@ -0,0 +1,10 @@
+#SOURCE: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+n = 3
+def sumZero(n):
+    lst = []
+    if n%2 != 0:
+        lst = [0]
+    for i in range (1,int(n//2)+1):
+        lst.extend([i,i*-1])
+    return lst
+print (sumZero(3))
