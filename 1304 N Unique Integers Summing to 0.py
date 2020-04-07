87e9e236a6f792fc8749b632ac09ae3b1588dc7d
Momin-C <hellomomins@yahoo.com>
2020-04-06 21:27:35 -0400

    Modified 1304

--- 1304 N Unique Integers Summing to 0.py
+++ 1304 N Unique Integers Summing to 0.py
@@ -4,7 +4,7 @@ def sumZero(n):
     lst = []
     if n%2 != 0:
         lst = [0]
-    for i in range (1,int(n//2)+1):
+    for i in range (1,int(n/2)+1):
         lst.extend([i,i*-1])
     return lst
 print (sumZero(3))
