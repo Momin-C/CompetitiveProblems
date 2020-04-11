60425d4dbd749eaabba8732815263140aa6980e3
Momin-C <hellomomins@yahoo.com>
2020-04-10 21:21:32 -0400

    Started working on 917

---
+++ 917 Reverse Only Letters.py
@@ -0,0 +1,15 @@
+#SOURCE: https://leetcode.com/problems/reverse-only-letters/
+S = "a-bC-dEf-ghIj" #j-Ih-gfE-dCba
+def reverseOnlyLetters(S):
+    """
+    :type S: str
+    :rtype: str
+    """
+    lst = []
+    for i in range (S.count("-")):
+        print(S.find("-",0))
+        lst.append(S.index("-"))
+        S = S[:i] + S[i+1:]
+        print (S)
+    #print (lst)
+print (reverseOnlyLetters(S))
