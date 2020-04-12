377001a9579a2af11789d80c695f987077a1c92f
Momin-C <hellomomins@yahoo.com>
2020-04-11 20:42:21 -0400

    Finished 227, must optimize for faster time

--- 227 Basic Calculator.py
+++ 227 Basic Calculator.py
@@ -1,10 +1,43 @@
 #SOURCE: https://leetcode.com/problems/basic-calculator-ii/
-strr = " 3  *      2/4 "
+s = " 3/2 "
 
-lst = (list(filter(lambda x: x != " ",strr)))
-i = 0
-
-if "/" in lst:
-    index = lst.index("/")
-    q = (int(lst[index-1]))/(int(lst[index+1]))
-print (q)
+lst = (list(filter(lambda x: x != " ",s)))
+def operation(sign):
+    where = lst.index(sign)
+    before = float(lst[where-1])
+    after = float(lst[where+1])
+    if sign == "/":
+        q = before/after
+    elif sign == "*":
+        q = before*after
+    elif sign == "+":
+        q = before+after
+    elif sign == "-":
+        q = before-after
+    lst.insert(where-1,q)
+    del lst[where-1:where+2]
+while len(lst) >1:
+    if "/" in lst:
+        operation("/")
+    if "*" in lst:
+        where = lst.index("*")
+        before = float(lst[where-1])
+        after = float(lst[where+1])
+        m = before*after
+        del lst[where-1:where+2]
+        lst.insert(where-1,m)
+    if "+" in lst:
+        where = lst.index("+")
+        before = float(lst[where-1])
+        after = float(lst[where+1])
+        p = before+after
+        del lst[where-1:where+2]
+        lst.insert(where-1,p)
+    if "-" in lst:
+        where = lst.index("-")
+        before = float(lst[where-1])
+        after = float(lst[where+1])
+        d = before-after
+        del lst[where-1:where+2]
+        lst.insert(where-1,d)
+print (lst)
