bf72a6d2e5e725aef4bdc029d7d5ffbc4b5cd0c3
Momin-C <hellomomins@yahoo.com>
2020-04-11 21:01:29 -0400

    Minor changes

--- 227 Basic Calculator.py
+++ 227 Basic Calculator.py
@@ -1,43 +1,36 @@
 #SOURCE: https://leetcode.com/problems/basic-calculator-ii/
 s = " 3/2 "
 
-lst = (list(filter(lambda x: x != " ",s)))
-def operation(sign):
-    where = lst.index(sign)
-    before = float(lst[where-1])
-    after = float(lst[where+1])
-    if sign == "/":
-        q = before/after
-    elif sign == "*":
-        q = before*after
-    elif sign == "+":
-        q = before+after
-    elif sign == "-":
-        q = before-after
-    lst.insert(where-1,q)
-    del lst[where-1:where+2]
-while len(lst) >1:
-    if "/" in lst:
-        operation("/")
-    if "*" in lst:
-        where = lst.index("*")
-        before = float(lst[where-1])
-        after = float(lst[where+1])
-        m = before*after
-        del lst[where-1:where+2]
-        lst.insert(where-1,m)
-    if "+" in lst:
-        where = lst.index("+")
-        before = float(lst[where-1])
-        after = float(lst[where+1])
-        p = before+after
-        del lst[where-1:where+2]
-        lst.insert(where-1,p)
-    if "-" in lst:
-        where = lst.index("-")
-        before = float(lst[where-1])
-        after = float(lst[where+1])
-        d = before-after
-        del lst[where-1:where+2]
-        lst.insert(where-1,d)
+def calculate(self, s):
+    lst = (list(filter(lambda x: x != " ",s)))
+    while len(lst) >1:
+        if "/" in lst:
+            where = lst.index("/")
+            before = float(lst[where-1])
+            after = float(lst[where+1])
+            q = before/after
+            del lst[where-1:where+2]
+            lst.insert(where-1,q)
+        if "*" in lst:
+            where = lst.index("*")
+            before = float(lst[where-1])
+            after = float(lst[where+1])
+            m = before*after
+            del lst[where-1:where+2]
+            lst.insert(where-1,m)
+        if "+" in lst:
+            where = lst.index("+")
+            before = float(lst[where-1])
+            after = float(lst[where+1])
+            p = before+after
+            del lst[where-1:where+2]
+            lst.insert(where-1,p)
+        if "-" in lst:
+            where = lst.index("-")
+            before = float(lst[where-1])
+            after = float(lst[where+1])
+            d = before-after
+            del lst[where-1:where+2]
+            lst.insert(where-1,d)
+    return (int(lst[0]))
 print (lst)
