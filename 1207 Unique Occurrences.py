cedcd4d0de20694b784fc5d7b7084bb46ce66a71
Momin-C <hellomomins@yahoo.com>
2020-04-12 23:55:44 -0400

    Optimized 1207 into 4 lines

--- 1207 Unique Occurrences.py
+++ 1207 Unique Occurrences.py
@@ -1,12 +1,7 @@
 arr = [-17,19,-17,-17,19,2,19,-17,19,19,2,19,0,19,19]
 def uniqueOccurrences(arr):
     lst = []
-    mod = list(set(arr))
-    for i in mod:
-        c = arr.count(i)
-        lst.append(c)
-    if sorted(set(lst)) == sorted(lst):
-        return True
-    else:
-        return False
+    for i in list(set(arr)):
+        lst.append(arr.count(i))
+    return True if sorted(set(lst)) == sorted(lst) else False
 print (uniqueOccurrences(arr))
