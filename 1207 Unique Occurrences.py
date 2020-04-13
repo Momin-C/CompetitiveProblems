c207c618b203ab2e384a949ed71f915a06b04344
Momin-C <hellomomins@yahoo.com>
2020-04-12 23:57:30 -0400

    Optimized 1207 even more

--- 1207 Unique Occurrences.py
+++ 1207 Unique Occurrences.py
@@ -3,5 +3,5 @@ def uniqueOccurrences(arr):
     lst = []
     for i in list(set(arr)):
         lst.append(arr.count(i))
-    return True if sorted(set(lst)) == sorted(lst) else False
+    return sorted(set(lst)) == sorted(lst)
 print (uniqueOccurrences(arr))
