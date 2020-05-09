238451ec508bacc28d2acf5899f7f9543622ccc6
Momin-C <hellomomins@yahoo.com>
2020-05-08 20:17:32 -0400

    Finished 1346

---
+++ 1346 Check If N and Its Double Exist.py
@@ -0,0 +1,9 @@
+arr = [-2,0,10,-19,4,6,-8]
+def checkIfExist(arr):
+    if arr.count(0) > 1:
+        return True
+    for i in arr:
+        if (i/2 in arr or i*2 in arr) and (i!=0):
+            return True
+    return False
+print(checkIfExist(arr))
