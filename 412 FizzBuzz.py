495d4109ec2b18745e3f3645498e5f73fab22557
Momin-C <hellomomins@yahoo.com>
2020-05-11 20:09:42 -0400

    Added second solution with list comprehensions

--- 412 FizzBuzz.py
+++ 412 FizzBuzz.py
@@ -1,6 +1,7 @@
 #PROBLEM 412
 #SOURCE: https://leetcode.com/problems/fizz-buzz/
 def fizzBuzz(n):
+    #SOLUTION 1
     lst = []
     for i in range(1,n+1):
         if i%3 ==0 and i%5==0:
@@ -11,5 +12,8 @@ def fizzBuzz(n):
             lst.append("Buzz")
         else:
             lst.append(str(i))
+
+    #SOLUTION 2 USING LIST COMPREHENSIONS
+    #lst = ["FizzBuzz" if (i%3==0 and i%5==0) else "Fizz" if i%3==0 else "Buzz" if i%5==0 else str(i) for i in range(1,n+1)]
     return lst
 print (fizzBuzz(15))
