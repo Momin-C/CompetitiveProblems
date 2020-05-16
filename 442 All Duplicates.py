7d515010890fd9dd13decba9418d05b6bd72bfd8
Momin-C <hellomomins@yahoo.com>
2020-05-16 17:18:58 -0400

    Finished 442

---
+++ 442 All Duplicates.py
@@ -0,0 +1,11 @@
+#PROBLEM 442
+#SOURCE: https://leetcode.com/problems/find-all-duplicates-in-an-array/
+nums = [4,3,2,7,8,2,3,1] #[2,3]
+def findDuplicates(nums):
+    lst = []
+    A = set(nums)
+    for i in A:
+        if nums.count(i) >=2:
+            lst.append(i)
+    return (lst)
+print (findDuplicates(nums))
--- 448 All Numbers Disappeared.py
+++ 448 All Numbers Disappeared.py
@@ -1,3 +1,5 @@
+#PROBLEM 448
+#SOURCE: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 nums = [4,3,2,7,8,2,3,1] #[5,6]
 def findDisappearedNumbers(nums):
     lst = []
