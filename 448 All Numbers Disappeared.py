9401b355888354d093feb45ae217796d53adc12e
Momin-C <hellomomins@yahoo.com>
2020-05-16 13:48:55 -0400

    Finished 448

---
+++ 448 All Numbers Disappeared.py
@@ -0,0 +1,9 @@
+nums = [4,3,2,7,8,2,3,1] #[5,6]
+def findDisappearedNumbers(nums):
+    lst = []
+    reduced = set(nums)
+    for i in range(1,len(nums)+1):
+        if i not in reduced:
+            lst.append(i)
+    return lst
+print (findDisappearedNumbers(nums))
--- WIP 47 Permuations II.py
+++ WIP 47 Permuations II.py
@@ -3,7 +3,6 @@
 nums = [1,1,2]
 def permuteUnique(nums):
     lst = []
-    q = len(nums)
     for i in range(len(nums)):
         if (nums[i:] + nums[:i]) not in lst:
             lst.append(nums[i:] + nums[:i])
